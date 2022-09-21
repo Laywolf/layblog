ee17bdfab6a4306f9c38040120b322f15ab2e560
Seongsu Kwon <seongsu_kwon@tmax.co.kr>
2022-09-21 12:25:23 +0900

    Add deploy workflow and apply prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production'
+const repository = 'layblog'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: !debug ? `/${respository}/` : '.',
+  trailingSlash: true,
+  images: {
+    loader: 'akamai',
+    path: '.',
+  },
 }
 
 module.exports = nextConfig
