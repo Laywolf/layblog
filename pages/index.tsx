a5a1a752551492c1f30d9c4e31c346cd64805d71
Seongsu Kwon <seongsu_kwon@tmax.co.kr>
2022-09-28 15:17:30 +0900

    Add youtube video on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,21 @@ export default function Home() {
         彼方花雪(かなた はなゆき、카나타 하나유키) 라는 <br />
         VRC 유저의 개인 블로그 입니다.
       </p>
+
+      <p className={styles.description}>好きな曲　&darr;</p>
+      <div className={styles['video-container']}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/2b1IexhKPz4"
+          title="YouTube video
+          player"
+          frameBorder="0"
+          allow="accelerometer; autoplay;
+          clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </>
   )
 }
