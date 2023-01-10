cd50d8ae2f173159cca333bef68ac200b9a2a1a7
Seongsu Kwon <seongsu_kwon@tmax.co.kr>
2023-01-10 17:55:03 +0900

    Fix css issue

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ import SideBar from './Sidebar'
 
 import styles from 'styles/components/Layout.module.css'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export const hiddenLayoutPages = ['api', 'photocard']
 
@@ -11,16 +12,21 @@ interface Props {
   children?: React.ReactNode
 }
 const Layout: React.FC<Props> = ({ children }) => {
-  const { pathname } = useRouter()
-  const page = pathname.split('/')[1]
+  const router = useRouter()
+  const page = router.pathname.split('/')[1]
   const hideLayout = hiddenLayoutPages.includes(page)
+
+  useEffect(() => {
+    const container = document.getElementById('container')
+    if (container) container.scrollTop = 0
+  }, [router])
 
   return hideLayout ? (
     <>{children}</>
   ) : (
     <>
       <Header />
-      <div className={styles.container}>
+      <div id="container" className={styles.container}>
         <main className={styles.main}>
           <section className={styles.section} style={{ width: '80vh' }}>
             {children}
